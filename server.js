3781b24ce0d80fe2450852d2c22c88ebebac494d
Guy Mazinter <gmazinter@gmail.com>
2018-12-24 20:42:20 +0200

    finished going through list of fixes

--- server.js
+++ server.js
@@ -6,8 +6,8 @@ const api = require('./server/routes/api')
 const request = require('request')
 const mongoose = require('mongoose')
 
-
-mongoose.connect('mongodb://localhost/weatherDB', {useNewUrlParser: true})
+mongoose.connect('process.env.mongodb://<dbuser>:<dbpassword>@ds243344.mlab.com:43344/weather-app-guy-db'||'mongodb://localhost/weatherDB');
+// mongoose.connect('mongodb://localhost/weatherDB', {useNewUrlParser: true})
 
 
 app.use(bodyParser.json())
@@ -17,7 +17,9 @@ app.use(express.static(path.join(__dirname, 'dist')))
 app.use(express.static(path.join(__dirname, 'node_modules')))
 
 
-const port = 8080
-app.listen(port, function() {
-    console.log(`Server up and running on port ${port}`)
-})
+app.listen(process.env.PORT || '8080');
+
+// const port = 8080
+// app.listen(port, function() {
+//     console.log(`Server up and running on port ${port}`)
+// })
